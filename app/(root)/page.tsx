12bdac9e846e8a811ed1135433a0b70c4710b110
sdgsnehal <sdgsnehal@gmail.com>
2024-02-03 16:06:44 +0530

    store setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,15 @@
+"use client"
+import { Modal } from "@/components/ui/modal";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+import { Children } from "react";
 
-export default function Home() {
+const SetupPage = () => {
   return (
-    <p>Hello Admin Dashboard</p>
+    <div><UserButton afterSignOutUrl="/"/>
+    <Modal title="test" description="test"isOpen onClose={()=>{}}>
+      Children
+      </Modal></div>
   );
 }
+export default SetupPage;
